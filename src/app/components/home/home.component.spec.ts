56b23f0dc86b4fefaa9598d45f7803ecf266b566
Denismay Concepcion <denismaycr@gmail.com>
2022-05-11 21:31:34 -0400

    0111052022

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
@@ -22,4 +22,10 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Comprobando que la variable no este indefinida', async() => {
+    const check = component.character$;
+    expect(check).not.toBeUndefined();
+  });
+
 });
